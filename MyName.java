ce9f2c8890f9f189f3a726c5925fc7d6e3f15815
halbaytm <65377064+halbaytm@users.noreply.github.com>
2020-05-14 18:46:35 -0700

    First HomeWork Task1

---
+++ MyName.java
@@ -0,0 +1,20 @@
+public class MyName {
+
+		public static void main(String[] args){
+
+
+			String firstName = "Hal";
+		    String lastName = "Muhammet";
+		    
+
+		System.out.println("My name is: "+firstName);
+		System.out.println("My last name is: "+lastName);
+
+
+    }
+
+
+
+
+
+}
