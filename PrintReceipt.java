cd8bc4d88b47425d038eab3acc1111152102d306
halbaytm <65377064+halbaytm@users.noreply.github.com>
2020-05-14 18:47:18 -0700

    First HomeWork Task2

---
+++ PrintReceipt.java
@@ -0,0 +1,26 @@
+public class PrintReceipt {
+
+	public static void main (String[] args){
+
+			String item1 = "980";
+		    String item2 = "40";
+
+		System.out.println("Computer: "+item1);
+		System.out.println("Headphones: "+item2);
+
+
+		int number1 = 980;
+		int number2;
+		number2 = 40;
+
+		int sum = number1 + number2;
+ 		System.out.println("Total:");
+		System.out.println(sum);
+
+	}
+
+
+
+
+
+}
